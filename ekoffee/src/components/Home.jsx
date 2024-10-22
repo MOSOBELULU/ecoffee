647ca53961ea70859ede958b39e39e081be4f508
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-10-22 10:48:00 +0200

    fetched data using api

--- ekoffee/src/components/Home.jsx
+++ ekoffee/src/components/Home.jsx
@@ -1,15 +1,38 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import classes from './Home.module.css'
 function Home(){
-    useEffect(() => {
-        fetch('https://fakestoreapi.com/products/category/jewelery')
-            .then(res => res.json())
-            .then(json => console.log(json))
-            .catch(error => console.error('Error fetching data:', error));
-    }, [])
-    
+    const [data, setData] = useState([])
+    const getData = async () => {
+        try {
+          const resp = await fetch('https://api.sampleapis.com/coffee/hot');
+          const json = await resp.json();
+          setData(json);
+        } catch (err) {
+            setData([]);
+          setData(err.message);
+        }
+      }
+      useEffect(() => {
+        getData();
+      }, []);
     return(
         <>
-        
+         <>
+            <h1>Hot Coffee Menu</h1>
+            <ul>
+                {data.length > 0 ? (
+                    data.map((item) => (
+                        <li key={item.id}>
+                            <img src={item.image} alt='coffeeImg' className={classes.coffeeImg} />
+                            <h3>{item.title}</h3>
+                            <p>{item.description}</p>
+                        </li>
+                    ))
+                ) : (
+                    <p>Loading or no data available</p>
+                )}
+            </ul>
+        </>
         </>
     )
 
