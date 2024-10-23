a7abc8089dbdeab9b2a5e2df706fc6fd59fa1e8a
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-10-23 10:10:13 +0200

    making a card

--- ekoffee/src/components/Navigation.jsx
+++ ekoffee/src/components/Navigation.jsx
@@ -4,6 +4,13 @@ function Navigation() {
     return (
         <header className={classes.header}>
             <img src='/images/koffee-Logo.png' className={classes.logo} alt='logo' />
+            <div className={classes.section}>
+                <h1>Welcome to Koffee</h1>
+                <p>Your coffee sanctuary</p>
+                <p>At Koffee, we craft every cup with care, offering a delightful range of flavors from bold and rich to smooth and delicate. Whether you’re starting your day or winding down, we have the perfect blend for you.</p>
+                <p>Our cozy atmosphere invites you to relax, connect with friends, or savor a quiet moment. With a welcoming smile and a passion for coffee, Koffee is your go-to spot for a memorable experience.</p>
+                <p>Join us today and discover the magic in every sip!</p>
+            </div>
             <div className={classes.navWrapper}>
         <nav className={classes.navbar}>
             <ul className={classes.navLinks}>
