1143260e6ea540bcd491c7de94701bbd21f381a3
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-10-23 20:38:35 +0200

    fixing bugs

--- ekoffee/src/components/CoffeeItem.jsx
+++ ekoffee/src/components/CoffeeItem.jsx
@@ -1,13 +1,15 @@
 /*eslint-disable*/
-import classes from './CoffeeItem.module.css';
-
 function CoffeeItem({ image, title, description }) {
     return (
-        <li className={classes.coffeeItem}>
-            <img src={image} alt={title} className={classes.coffeeImg} />
-            <h3>{title}</h3>
-            <p>{description}</p>
+        < >
+        <li className="bg-white shadow-md rounded-lg overflow-hidden transform transition duration-500 hover:scale-105 max-w-xs">
+            <img src={image} alt={title} className="w-64 h-48 object-cover mx-auto" />
+            <div className="p-2">
+                <h3 className="text-xl font-semibold mb-2">{title}</h3>
+                <p className="text-gray-600">{description}</p>
+            </div>
         </li>
+        </>
     );
 }
 
