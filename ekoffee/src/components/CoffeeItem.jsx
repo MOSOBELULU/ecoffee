bdae8d0f100c054bb306db5eda751f98e7487cf9
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-10-31 11:14:24 +0200

    wip

--- ekoffee/src/components/CoffeeItem.jsx
+++ ekoffee/src/components/CoffeeItem.jsx
@@ -1,12 +1,12 @@
 /*eslint-disable*/
 function CoffeeItem({ image, title, description }) {
     return (
-      <ul >
+      <ul className="grid grid-cols-1  gap-2 p-6">
         <li className="bg-white shadow-md rounded-lg overflow-hidden transform transition duration-500 hover:scale-105">
           <img src={image} alt={title} className="w-full h-48 object-cover" />
           <div className="p-4">
             <h3 className="text-xl font-semibold mb-2">{title}</h3>
-            <p className="text-gray-600">{description}</p>
+            {/* <p className="text-gray-600">{description}</p> */}
           </div>
         </li>
       </ul>
