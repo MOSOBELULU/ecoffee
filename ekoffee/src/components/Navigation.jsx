edf6a84746f0513515fc274dbb92f42a1ec7dd12
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-11-05 13:04:57 +0200

    added more pages

--- ekoffee/src/components/Navigation.jsx
+++ ekoffee/src/components/Navigation.jsx
@@ -6,10 +6,15 @@ function Navigation() {
             <img src='/images/koffee-Logo.png' className={classes.logo} alt='logo' />
             <div className={classes.section}>
                 <h1>Welcome to Koffee</h1>
+                <br></br>
                 <p>Your coffee sanctuary</p>
+                <br></br>
                 <p>At Koffee, we craft every cup with care, offering a delightful range of flavors from bold and rich to smooth and delicate. Whether you’re starting your day or winding down, we have the perfect blend for you.</p>
+                <br></br>
                 <p>Our cozy atmosphere invites you to relax, connect with friends, or savor a quiet moment. With a welcoming smile and a passion for coffee, Koffee is your go-to spot for a memorable experience.</p>
                 <p>Join us today and discover the magic in every sip!</p>
+                <br></br>
+                <button className={classes.btnExplore}>Explore</button>
             </div>
             <div className={classes.navWrapper}>
         <nav className={classes.navbar}>
@@ -17,7 +22,6 @@ function Navigation() {
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Menu</a></li>
                     <li><a href="#">Contact</a></li>
-                    <li><a href="#">Shop/Cart</a></li>
                     <li><a href="#">Gallery</a></li>
                 </ul>
         </nav>
