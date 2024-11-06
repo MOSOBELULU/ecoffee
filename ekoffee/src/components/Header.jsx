8af39a64eab6db10019da719476c1f6821b1d35c
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-11-06 12:57:48 +0200

    wip

--- ekoffee/src/components/Header.jsx
+++ ekoffee/src/components/Header.jsx
@@ -1,22 +1,19 @@
-import classes from 'Header.module.css'
+import classes from './Header.module.css';
+
 function Header() {
-    return(
+    return (
         <div className={classes.header}>
-          <div className={classes.section}>
+            <div className={classes.section}>
                 <h1>Welcome to Koffee</h1>
-                <br></br>
                 <p>Your coffee sanctuary</p>
-                <br></br>
                 <p>At Koffee, we craft every cup with care, offering a delightful range of flavors from bold and rich to smooth and delicate. Whether you’re starting your day or winding down, we have the perfect blend for you.</p>
-                <br></br>
                 <p>Our cozy atmosphere invites you to relax, connect with friends, or savor a quiet moment. With a welcoming smile and a passion for coffee, Koffee is your go-to spot for a memorable experience.</p>
                 <p>Join us today and discover the magic in every sip!</p>
-                <br></br>
                 <button className={classes.btnExplore}>Explore</button>
             </div>
-        <img src='/images/navPic.jpg' className={classes.navPic} alt='navPic' />
+            <img src='/images/navPic.jpg' className={classes.navPic} alt='navPic' />
         </div>
-    )
-
+    );
 }
+
 export default Header;
--- ekoffee/src/components/Home.jsx
+++ ekoffee/src/components/Home.jsx
@@ -1,9 +1,12 @@
-import Header from "./Header";
+import Header from './Header'
+
 function Home() {
    
 
     return (
+        <>
         <Header />
+        </>
     )
 }
 
